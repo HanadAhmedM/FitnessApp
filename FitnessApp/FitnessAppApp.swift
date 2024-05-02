b1a33c34dc27e69983f05ce474079b5ece31b22a
HanadAhmedM <hanadahmed1414@gmail.com>
2024-05-02 14:53:34 +0200

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Hanad.Ahmed on 2024-05-02.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
