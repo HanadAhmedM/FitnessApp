f2c11c15a12434937d8266f3d3ce3e4ad6c9fcf3
HanadAhmedM <hanadahmed1414@gmail.com>
2024-05-06 10:10:40 +0200

    Register Page Design

---
+++ FitnessApp/RegisterPage.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterPage.swift
+//  FitnessApp
+//
+//  Created by Hanad.Ahmed on 2024-05-06.
+//
+
+import SwiftUI
+
+struct RegisterPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterPage()
+}
