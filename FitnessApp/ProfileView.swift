531447f6d76bba397933547d48d096dc519e1734
aelin6277 <aelin6277@gmail.com>
2024-05-21 09:42:36 +0200

    med scrollbar lista i profile

--- FitnessApp/ProfileView.swift
+++ FitnessApp/ProfileView.swift
@@ -16,50 +16,54 @@ struct ProfileView: View {
             ZStack {
                 Color(red: 27/255, green: 178/255, blue: 115/255).ignoresSafeArea()
                 VStack {
+                  
+
                     Text("Profile")
                         .font(.system(size: 24, weight: .bold))
-                        .padding(.top, 100)
+                        .padding(.top, 20)
                     
                     Spacer()
                     
-                    VStack(spacing: 10) {
-                        ForEach(workoutData.selectedExercises.keys.sorted(), id: \.self) { key in
-                            VStack(alignment: .leading) {
-                                Text(key)
-                                    .font(.headline)
-                                    .foregroundColor(.black)
-                                ForEach(workoutData.selectedExercises[key] ?? []) { workout in
-                                    VStack(alignment: .leading) {
-                                        HStack {
-                                            Text(workout.name)
-                                                .font(.headline)
-                                                .foregroundColor(.black)
-                                            Spacer()
-                                            Button(action: {
-                                                deleteWorkout(day: key, workout: workout)
-                                            }) {
-                                                Image(systemName: "trash")
-                                                    .foregroundColor(.red)
+                    ScrollView { // Lägg till ScrollView här
+                        VStack(spacing: 10) {
+                            ForEach(workoutData.selectedExercises.keys.sorted(), id: \.self) { key in
+                                VStack(alignment: .leading) {
+                                    Text(key)
+                                        .font(.headline)
+                                        .foregroundColor(.black)
+                                    ForEach(workoutData.selectedExercises[key] ?? []) { workout in
+                                        VStack(alignment: .leading) {
+                                            HStack {
+                                                Text(workout.name)
+                                                    .font(.headline)
+                                                    .foregroundColor(.black)
+                                                Spacer()
+                                                Button(action: {
+                                                    deleteWorkout(day: key, workout: workout)
+                                                }) {
+                                                    Image(systemName: "trash")
+                                                        .foregroundColor(.red)
+                                                }
+                                            }
+                                            ForEach(workout.exercises) { exercise in
+                                                Text(exercise.name)
+                                                    .foregroundColor(.gray)
                                             }
                                         }
-                                        ForEach(workout.exercises) { exercise in
-                                            Text(exercise.name)
-                                                .foregroundColor(.gray)
-                                        }
+                                        .padding()
+                                        .background(Color.white)
+                                        .cornerRadius(10)
                                     }
-                                    .padding()
-                                    .background(Color.white)
-                                    .cornerRadius(10)
                                 }
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
                             }
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(10)
                         }
+                        .padding(.horizontal, 20)
+                        .background(Color.white)
+                        .cornerRadius(30)
                     }
-                    .padding(.horizontal, 20)
-                    .background(Color.white)
-                    .cornerRadius(30)
                     
                     Spacer()
                 }
