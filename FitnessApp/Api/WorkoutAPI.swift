bada6358cfe0616ff0cbacdf836a7314002c7099
HanadAhmedM <hanadahmed1414@gmail.com>
2024-05-23 15:36:02 +0200

    WorkoutApi och fixa design

---
+++ FitnessApp/Api/WorkoutAPI.swift
@@ -0,0 +1,8 @@
+//
+//  WorkoutAPI.swift
+//  FitnessApp
+//
+//  Created by Hanad.Ahmed on 2024-05-22.
+//
+
+import Foundation
---
+++ FitnessApp/Model/Exercise.swift
@@ -0,0 +1,8 @@
+//
+//  Exercise.swift
+//  FitnessApp
+//
+//  Created by Hanad.Ahmed on 2024-05-22.
+//
+
+import Foundation
